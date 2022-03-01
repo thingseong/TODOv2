6cb5751027e0eb470b37d34c11770dfdf665e22b
thingseong <audtjd1893@gmail.com>
2022-03-01 10:29:46 +0900

    add Clock

---
+++ src/components/clock.js
@@ -0,0 +1,43 @@
+import Component from "../core/Component.js";
+
+
+export default class Clock extends Component{
+
+    setup(){
+        this.$state ={
+            time : new Date().toLocaleTimeString()
+        }
+        
+
+    }
+
+    template(){
+        const {time} = this.$state;
+        return`
+            <a>
+            ${time}
+            </a>
+        `
+    }
+
+    mounted(){
+        
+    }
+
+    setEvent(){
+        setInterval(()=>{
+            let {time} = this.$state;
+            time = new Date().toLocaleTimeString();
+            this.setState({time});
+        }, 1000);
+
+    }
+
+
+
+
+
+
+
+
+}
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,18 @@
 import Component from "../core/Component.js";
+import Clock from "./clock.js";
 
 export default class Footer extends Component{
     template(){
         return `
             <div>
+                <div data-component="clock"></div>
                 <a id='madeby'>Made by thingseong</a>
             </div>
         `;
     }
+    mounted(){
+        const $clock = this.$target.querySelector('[data-component="clock"]');
+        new Clock($clock, {});
+    }
+
 }
